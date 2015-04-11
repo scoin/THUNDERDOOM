a647b8b7e2fe519a9b3c67d9b11c3870c449233b
Armen Vartan <vartan.armen@gmail.com>
2015-04-11 17:49:12 -0400

    attached socket.io to server

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var io = require('socket.io')()
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.io = io
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

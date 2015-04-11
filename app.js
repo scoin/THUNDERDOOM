bd89ed4d5754f2a6134100e4a263a2f32fed2307
Armen Vartan <vartan.armen@gmail.com>
2015-04-11 18:46:24 -0400

    injected socket.io into route

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', routes(app.io));
 app.use('/users', users);
 
 // catch 404 and forward to error handler

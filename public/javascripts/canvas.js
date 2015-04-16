21e3db9034e3f20461e2c605d9d0e36aa933a703
Armen Vartan <vartan.armen@gmail.com>
2015-04-15 20:51:50 -0400

    white space

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -15,12 +15,12 @@ Canvas.prototype.drawPlayer = function(player){
 }
 
 Canvas.prototype.drawProjectile = function(projectile){
-    var canvas = this;
-    canvas.fgCtx.beginPath();
-    canvas.fgCtx.arc(projectile.x, projectile.y, projectile.size, 8, 5 * Math.PI);
-    var grd = canvas.fgCtx.createRadialGradient(projectile.x, projectile.y, projectile.size, projectile.x + projectile.size, projectile.y + projectile.size, projectile.size);
-    grd.addColorStop(0, '#FFCC5E');
-    grd.addColorStop(1, 'white');
-    canvas.fgCtx.fillStyle=grd;
-    canvas.fgCtx.fill();
+  var canvas = this;
+  canvas.fgCtx.beginPath();
+  canvas.fgCtx.arc(projectile.x, projectile.y, projectile.size, 8, 5 * Math.PI);
+  var grd = canvas.fgCtx.createRadialGradient(projectile.x, projectile.y, projectile.size, projectile.x + projectile.size, projectile.y + projectile.size, projectile.size);
+  grd.addColorStop(0, '#FFCC5E');
+  grd.addColorStop(1, 'white');
+  canvas.fgCtx.fillStyle=grd;
+  canvas.fgCtx.fill();
 }
